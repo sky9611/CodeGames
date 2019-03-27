0ea96deedea30f5f82bf83d4a5a262ad90eccf2b
twu <collinbeyond@gmail.com>
2019-03-27 13:23:06 +0100

    [110]

---
+++ leetcode/DFS/[110] Balanced Binary Tree/110.balanced-binary-tree.cs
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=110 lang=csharp
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (40.49%)
+ * Total Accepted:    302.7K
+ * Total Submissions: 746.7K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ * 
+ * For this problem, a height-balanced binary tree is defined as:
+ * 
+ * 
+ * a binary tree in which the depth of the two subtrees of every node never
+ * differ by more than 1.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Given the following tree [3,9,20,null,null,15,7]:
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * Return true.
+ * 
+ * Example 2:
+ * 
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ * 
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ * 
+ * 
+ * Return false.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public bool IsBalanced(TreeNode root) {
+        if (root == null) return true;
+        if (Math.Abs(Foo(root.left, 0) - Foo(root.right, 0)) > 1) return false;
+        return IsBalanced(root.left)&&IsBalanced(root.right);
+    }
+    
+    private int Foo(TreeNode node, int height){
+        if (node == null) return height;
+        return Math.Max(Foo(node.left, height+1),Foo(node.right, height+1));
+    }
+}
+
