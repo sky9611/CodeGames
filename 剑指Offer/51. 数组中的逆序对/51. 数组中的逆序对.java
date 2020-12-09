a7220110a37478f5a73245786d2759f7d4c35183
Tianhao WU <collinbeyond@gmail.com>
2020-12-09 14:04:18 +0800

    51 归并法

--- 剑指Offer/51. 数组中的逆序对/51. 数组中的逆序对.java
+++ 剑指Offer/51. 数组中的逆序对/51. 数组中的逆序对.java
@@ -1,24 +1,63 @@
 class Solution {
     public int reversePairs(int[] nums) {
-        // 离散化
-        Set<Integer> set = new TreeSet<Integer>();
-        for(int num : nums)
-            set.add(num);
+        // // 树状数组
+        // // 离散化
+        // Set<Integer> set = new TreeSet<Integer>();
+        // for(int num : nums)
+        //     set.add(num);
         
-        Map<Integer, Integer> map = new HashMap<>();
-        int idx = 1;
-        for(int num : set)
-            map.put(num, idx++);
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int idx = 1;
+        // for(int num : set)
+        //     map.put(num, idx++);
 
-        int res = 0;
-        BIT bit = new BIT(map.size());
-        for(int i = 0; i < nums.length; i++){
-            int left = map.get(nums[i]);
-            res += i - bit.getSum(left);
-            bit.update(left, 1);
+        // int res = 0;
+        // BIT bit = new BIT(map.size());
+        // for(int i = 0; i < nums.length; i++){
+        //     int left = map.get(nums[i]);
+        //     res += i - bit.getSum(left);
+        //     bit.update(left, 1);
+        // }
+
+        // return res;
+
+        // 归并
+        return reversePairs(nums, 0, nums.length-1, new int[nums.length]);
+    }
+
+    private int reversePairs(int[] nums, int start, int last, int[] tmp){
+        if(start >= last) return 0;
+        int mid = start + (last - start) / 2;
+        int leftCount = reversePairs(nums, start, mid, tmp);
+        int rightCount = reversePairs(nums, mid + 1, last, tmp);
+
+        if(nums[mid] <= nums[mid + 1])
+            return leftCount + rightCount;
+        
+        int crossCount = mergeCount(nums, start, mid, last, tmp);
+        return leftCount + crossCount + rightCount;
+    }
+
+    private int mergeCount(int[] nums, int start, int mid, int last, int[] tmp){
+        int i = start, j = mid + 1, k = 0, count = 0;
+        while(i <= mid && j <= last){
+            if(nums[i] <= nums[j])
+                tmp[k++] = nums[i++];
+            else{
+                count += mid - i + 1;
+                tmp[k++] = nums[j++];
+            }
+        }
+        while(i <= mid)
+            tmp[k++] = nums[i++];
+        while(j <= last)
+            tmp[k++] = nums[j++];
+
+        for(i = start; i <= last; i++){
+            nums[i] = tmp[i-start];
         }
 
-        return res;
+        return count;
     }
 }
 
