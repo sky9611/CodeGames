bd16a57397b8a164db933d70b51536c31ec4a266
Tianhao WU <collinbeyond@gmail.com>
2020-12-10 11:37:11 +0800

    486

--- leetcode/[486] 预测赢家/486.预测赢家.java
+++ leetcode/[486] 预测赢家/486.预测赢家.java
@@ -54,7 +54,22 @@
 // @lc code=start
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
+        int length = nums.length;
+        // dp[i][j]表示甲先手面对区间[i...j]时，甲对乙的净胜分
+        // 最终求的就是，甲先手面对区间[0...n-1]时，甲对乙的净胜分dp[0][n-1]是否>=0。
+        // 甲先手面对区间[i...j]时，
+        //     - 如果甲拿nums[i]，那么变成乙先手面对区间[i+1...j]，这段区间内乙对甲的净胜分为dp[i+1][j]；
+        //       那么甲对乙的净胜分就应该是nums[i] - dp[i+1][j]。
+        //     - 如果甲拿nums[j]，同理可得甲对乙的净胜分为是nums[j] - dp[i][j-1]。
+        int[][] dp = new int[length][length];
+        for(int j = 0; j < length; j++){
+            dp[j][j] = nums[j];
+            for(int i = j-1; i >= 0; i--){
+                dp[i][j] = Math.max(nums[i] - dp[i+1][j], nums[j] - dp[i][j-1]);
+            }
+        }
 
+        return dp[0][length-1] >= 0;
     }
 }
 // @lc code=end
