0260029ddc3a29d9c8eb7992e23af7d3c315927f
Tianhao WU <collinbeyond@gmail.com>
2020-12-16 23:49:01 +0800

    baseMToBaseN

---
+++ InterestingAlgos/BaseMToBaseN/src/baseMToBaseN.java
@@ -0,0 +1,35 @@
+public class main {
+    static final char[] charDict = new char[]{'0','1','2','3','4','5','6','7','8','9','A', 'B', 'C', 'D', 'E', 'F'};
+
+    public static void main(String[] args){
+        String number = "465AB";
+        int m = 16;
+        int n = 10;
+        System.out.println(toDigits(fromDigits(number, m), n));
+    }
+
+    // Convert a number N to its decimal representation in base b
+    public static String toDigits(int N, int b){
+        int originN = N;
+        StringBuffer sb = new StringBuffer();
+        while(N > 0){
+            sb.append(charDict[N % b]);
+            N /= b;
+        }
+        if(originN < 0) sb.append("-");
+        return sb.reverse().toString();
+    }
+
+    // Compute the number N in base b to decimal
+    public static int fromDigits(String N, int b){
+        int res = 0;
+
+        int i = N.charAt(0) == '-' ? 1 : 0;
+        for(;i < N.length(); i++){
+            char digit = N.charAt(i);
+            res = res * b + digit < 'A' ? digit - '0' : 10 + digit - 'A';
+        }
+
+        return N.charAt(0) == '-' ? -res : res;
+    }
+}
