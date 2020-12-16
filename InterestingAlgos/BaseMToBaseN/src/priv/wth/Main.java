cfeca1d8825bc0f5e9043c2410c618f81c82aaa5
Tianhao WU <collinbeyond@gmail.com>
2020-12-16 23:49:43 +0800

    baseMToBaseN

--- InterestingAlgos/BaseMToBaseN/src/priv/wth/Main.java
+++ InterestingAlgos/BaseMToBaseN/src/priv/wth/Main.java
@@ -1,10 +1,13 @@
-public class main {
+package priv.wth;
+
+public class Main {
+
     static final char[] charDict = new char[]{'0','1','2','3','4','5','6','7','8','9','A', 'B', 'C', 'D', 'E', 'F'};
 
     public static void main(String[] args){
-        String number = "465AB";
-        int m = 16;
-        int n = 10;
+        String number = "-465AB";
+        int m = 13;
+        int n = 2;
         System.out.println(toDigits(fromDigits(number, m), n));
     }
 
@@ -12,6 +15,7 @@ public class main {
     public static String toDigits(int N, int b){
         int originN = N;
         StringBuffer sb = new StringBuffer();
+        N = Math.abs(N);
         while(N > 0){
             sb.append(charDict[N % b]);
             N /= b;
@@ -27,9 +31,9 @@ public class main {
         int i = N.charAt(0) == '-' ? 1 : 0;
         for(;i < N.length(); i++){
             char digit = N.charAt(i);
-            res = res * b + digit < 'A' ? digit - '0' : 10 + digit - 'A';
+            res = res * b + (digit < 'A' ? (digit - '0') : (10 + digit - 'A'));
         }
 
-        return N.charAt(0) == '-' ? -res : res;
+        return N.charAt(0) == '-' ? (-res) : res;
     }
 }
